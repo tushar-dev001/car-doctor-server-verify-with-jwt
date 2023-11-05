979006930cf793201a66c4f56d7f536f5fd3be8e
tushar-dev001 <tushar789imran@gmail.com>
2023-11-05 18:24:03 +0600

    this project is done

---
+++ __deploy__.js
@@ -0,0 +1,7 @@
+/**
+ * server site deploy
+ *  1. vercel config
+ * 2. if you use cookies in cors site: use for your production url
+ * 3. environment variable
+ * 4. 
+*/
---
+++ __jwt__token__.js
@@ -0,0 +1,15 @@
+/**
+ * 1. jwt --> json web token
+ * 2. generate a token by using jwt.sign
+ * 3. create api set to cookie. http only, secure, sameSite.
+ * 4. from client side: axios withCredential true
+ * 5. cors setup origin and credentials: true
+*/
+
+/**
+ * 1. for secure api calls
+ * 2. server side: install cookie parser and use it as a middleware
+ * 3. req.cookies
+ * 4. on the client side: make api call using axios withCredentials: true or( credentials include while using fetch)
+ * 5. 
+ * */ 
